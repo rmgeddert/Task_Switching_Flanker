ff32c0b882dba522d7c5666058c769175a358935
rmgeddert <54999346+rmgeddert@users.noreply.github.com>
2019-10-03 16:38:39 -0400

    Reworked code structure
    Split most important steps (prepare, instructions, practice,main task) into separate files. potentially will recombine depending on length, but I like appeal of short main function that calls the various sub procedures.

---
+++ instructions.js
@@ -0,0 +1,45 @@
+// initiate variables
+var instrNum = 1; instrNumMax = 8
+
+// show instructions to participant (one at a time)
+function showInstructions(){
+  if (instrNum == instrNumMax){
+    $('#instructions').show();
+    $('#nextInstrButton').hide();
+    $('#startExpButton').show();
+  };
+
+  function addInstructions(slideNum){
+    switch(slideNum){
+      case 1:
+        return "In this task, you will see a number (1-9 excluding 5) appear in the middle of the screen.";
+      case 2:
+        return "Before each trial, a cue will appear telling you what to do on the next task.";
+      case 3:
+        return "If you see 'Larger/Smaller', then when you see the number, indicate if it is larger or smaller than five (5) using the 'N' and 'M' keys. Use your RIGHT hand index and middle finger to respond.";
+      case 4:
+        return "If you see 'Odd/Even', then when you see the number, indicate if it is odd or even using the 'Z' and 'X' keys. Use your LEFT hand index and middle finger to respond.";
+      case 5:
+        return "Additionally, there will be two (2) numbers on the left and two (2) numbers on the right of the center number. Try to ignore these; you should only respond with respect to the middle number.";
+      case 6:
+        return "It is important that you respond as quickly and as accurately as possible.";
+      case 7:
+        return "Please enlarge this window to your entire screen and sit a comfortable distance from the computer screen. You must get above 80% on the practice task in order to move onto the main task.";
+      case 8:
+        return "You will start with a practice block of 20 trials. Press 'Start Experiment' to begin."
+    };
+  };
+
+  // when running from Atom, need to use $(document).on, when running via the Duke Public Home Directory, either seems fine. See https://stackoverflow.com/questions/19237235/jquery-button-click-event-not-firing for details //
+  $(document).on('click', "#nextInstrButton", function(){
+  // $("#nextInstrButton").on('click', function(){
+    if (instrNum < instrNumMax){
+      $('#instructions' + instrNum).text(addInstructions(instrNum));
+      instrNum++;
+    } else{
+      $('#instructions' + instrNum).text(addInstructions(instrNum));
+      $('#nextInstrButton').hide();
+      $('#startExpButton').show();
+    };
+  });
+};
---
+++ prepareTask.js
@@ -0,0 +1,30 @@
+function prepare(){
+  loadImages();
+};
+
+function loadImages(){
+  var imgFile = listImageNames;
+  var imgArr = new Array(imgFile.length);
+  var imgCount = 0;
+
+  // load images from images.js into local array imgArr
+  while (imgCount < imgFile.length){
+    imgArr[imgCount] = new Image();
+    imgArr[imgCount].src = imgFile[imgCount];
+    imgCount++;
+  }
+
+  // randomizes image array
+  random_img = []
+  for (var g=0;g < imgFile.length;g++) {
+    random_img.push(g);
+  }
+
+  shuffle(random_img);
+}
+
+// Fisher-Yates shuffle
+function shuffle(array){
+  for(var j, x, i = array.length; i; j = Math.floor(Math.random() * i), x = array[--i], array[i] = array[j], array[j] = x);
+  return array;
+}
