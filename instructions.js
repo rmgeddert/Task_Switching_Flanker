417c4121376451859fe9b9a1224992b0b8f2eb09
rmgeddert <54999346+rmgeddert@users.noreply.github.com>
2019-10-07 12:08:38 -0400

    Update instructions.js
    added "break;" to each line

--- instructions.js
+++ instructions.js
@@ -13,20 +13,28 @@ function showInstructions(){
     switch(slideNum){
       case 1:
         return "In this task, you will see a number (1-9 excluding 5) appear in the middle of the screen.";
+        break;
       case 2:
         return "Before each trial, a cue will appear telling you what to do on the next task.";
+        break;
       case 3:
         return "If you see 'Larger/Smaller', then when you see the number, indicate if it is larger or smaller than five (5) using the 'N' and 'M' keys. Use your RIGHT hand index and middle finger to respond.";
+        break;
       case 4:
         return "If you see 'Odd/Even', then when you see the number, indicate if it is odd or even using the 'Z' and 'X' keys. Use your LEFT hand index and middle finger to respond.";
+        break;
       case 5:
         return "Additionally, there will be two (2) numbers on the left and two (2) numbers on the right of the center number. Try to ignore these; you should only respond with respect to the middle number.";
+        break;
       case 6:
         return "It is important that you respond as quickly and as accurately as possible.";
+        break;
       case 7:
         return "Please enlarge this window to your entire screen and sit a comfortable distance from the computer screen. You must get above 80% on the practice task in order to move onto the main task.";
+        break;
       case 8:
-        return "You will start with a practice block of 20 trials. Press 'Start Experiment' to begin."
+        return "You will start with a practice block of 20 trials. Press 'Start Experiment' to begin.";
+        break;
     };
   };
 
