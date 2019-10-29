c9ffdb5743cb89c800e26a456032d4de4b027ba4
Raphael Geddert <rmgeddert@gmail.com>
2019-10-29 09:47:04 -0400

    Reorganize Code

--- main.js
+++ main.js
@@ -2,29 +2,14 @@
 "use strict";
 
 //declare global variables (easy access for changes)
-let trialNumber = 320;
-let miniBlockSize = 16;
 
-// ----- Prepare Experiment ----- //
-// let stimCategories = {
-//   Parity: {},
-//   Magnitude: {},
-//   CongruencyClass: {}
-// };
-// let stimulusArray = createStimArray(); //also defines stimCategories
-// let cuedTaskArray = createCuedTaskArray();
-// let actionArray = createActionArray();
-// console.log(stimulusArray);
-// console.log(cuedTaskArray);
-// console.log(actionArray);
-
-console.log(stimArray);
-
-// ----- Instructions ----- //
-runInstructions();
-
-// ----- Practice Block ----- //
 $(document).ready(function(){
+  // ----- Prepare Stimulis ----- //
+  let stimArray = createStimArray();
+  let stimClassification = defineStimulusTypes(stimArray);
+
+  // ----- Practice Block ----- //
+  runInstructions();
   let taskCanvas = document.getElementById('myCanvas');
   let ctx = taskCanvas.getContext('2d');
 });
--- prepareTask2.js
+++ prepareTask2.js
@@ -1,53 +1,53 @@
 // define possivle stimuli
-let congruentStim = ['11111' , '22222' , '33333' , '44444' , '666666' , '77777' , '88888' , '99999'];
+let congruentStim = ['11111' , '22222' , '33333' , '44444' , '66666' , '77777' , '88888' , '99999'];
 let parityIncStim = [ ['22122','44144'] , ['11211','33233'] , ['22322','44344'] , ['11411','33433'] , ['77677','99699'] , ['66766','88788'] , ['77877','99899'] , ['66966','88988'] ];
 let magnitudeIncStim = [ ['77177','99199'] , ['66266','88288'] , ['77377','99399'] , ['66466','88488'] , ['22622','44644'] , ['11711','33733'] , ['22822','44844'] , ['11911','33933'] ];
 
-/* Define each stimulus in an stimulus classification objects
-This stores distractor number, stim number, stim magnitude, stim parity,
-and stim congruency to prevent repeats in task set.*/
-
-let stimArray = createStimArray();
-let stimTypes = {
-  target: {},
-  targetPar: {},
-  targetMag: {},
-  distractor: {},
-  congruency: {}
-};
-defineStimulusTypes(stimArray, incongruency);
-
-function defineStimulusTypes(inputArr){
-  inputArr.forEach(function(newStim){
-    // pull out target and distractor from stimulus string
-    let stimTarget = newStim.slice(2,3); // target = 3rd number in string
-    let stimDistractor = newStim.slice(0,1); // distractor = 1st number in string
-
-    // add stimulus variables to stimTypes variable
-    stimTypes['target'][newStim] = stimTarget;
-    stimTypes['distractor'][newStim] = stimDistractor;
-    stimTypes['targetPar'][newStim] = (isEven(stimTarget)) ? 'even' : 'odd';
-    stimTypes['targetMag'][newStim] = (stimTarget > 5) ? 'larger' : 'smaller';
-    stimTypes['congruency'][newStim] = function(incongruency){
-      if incongruency = ""
-    }
-  })
-  console.log(stimTypes);
-}
-
 function createStimArray() {
   let stimArray = [];
-  
+
   //select stim from congruent and incongruent stim arrays and add to main stim array
-  congruentStim.forEach(function(stim){stimArray.push(stim);})
-  parityIncStim.forEach(function(stimPair,"parity"){
-    stimArray.push(stimPair[getRandomInt(2)]);
+  congruentStim.forEach( function(stim){
+    stimArray.push( [stim, "congruent"] );
   })
-  magnitudeIncStim.forEach(function(stimPair,"parity"){
-    stimArray.push(stimPair[getRandomInt(2)]);
+
+  parityIncStim.forEach( function(stimPair){
+    stimArray.push( [stimPair[getRandomInt(2)], "parityInc"] );
+  })
+
+  magnitudeIncStim.forEach( function(stimPair){
+    stimArray.push( [stimPair[getRandomInt(2)], "magnitudeInc"] );
   })
 
   return stimArray;
+}
+
+function defineStimulusTypes(inputArr){
+  // object var for storing stimulus information
+  let stimTypes = {
+    target: {},
+    targetPar: {},
+    targetMag: {},
+    distractor: {},
+    congruency: {}
+  };
+
+  inputArr.forEach(function(newStim){
+    let stimulus = newStim[0];
+
+    // pull out target and distractor from stimulus string
+    let stimTarget = stimulus.slice(2,3); // target = 3rd number in string
+    let stimDistractor = stimulus.slice(0,1); // distractor = 1st number in string
+
+    // add stimulus variables to stimTypes variable
+    stimTypes['target'][stimulus] = stimTarget;
+    stimTypes['distractor'][stimulus] = stimDistractor;
+    stimTypes['targetPar'][stimulus] = (isEven(stimTarget)) ? 'even' : 'odd';
+    stimTypes['targetMag'][stimulus] = (stimTarget > 5) ? 'larger' : 'smaller';
+    stimTypes['congruency'][stimulus] = newStim[1];
+  })
+
+  return stimTypes;
 }
 
 // ------------- Misc Functions ------------- //
